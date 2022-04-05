e80720ceb8fb97ee0b0f44d3d3ea104cb5f41561
Tomasz <tomigunpl@icloud.com>
2022-04-05 19:00:42 +0200

    Type of variables

--- resources/lecture3/script.js
+++ resources/lecture3/script.js
@@ -1,4 +1,4 @@
-let firstName = "Tomasz";
+/*let firstName = "Tomasz";
 console.log(firstName);
 
 let country = "Poland";
@@ -6,3 +6,19 @@ let population = 39;
 
 console.log(country);
 console.log(population);
+*/
+
+let javafun = true;
+console.log(javafun);
+console.log(typeof javafun);
+
+javafun = "yes";
+console.log(typeof javafun);
+console.log(javafun);
+
+let year;
+console.log(typeof year);
+
+year = 2137;
+console.log(typeof year);
+console.log(year);
