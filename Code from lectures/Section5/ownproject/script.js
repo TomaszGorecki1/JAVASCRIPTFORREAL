782f44d5afbcd42e75866b1b97d366c8b3311a78
Tomasz <tomigunpl@icloud.com>
2022-04-14 20:41:40 +0200

    Adding to own project button with additional info

--- Code from lectures/Section5/ownproject/script.js
+++ Code from lectures/Section5/ownproject/script.js
@@ -30,3 +30,33 @@ document.querySelector(`.submit`).addEventListener('click', function () {
   document.querySelector(`.grade`).value = '';
   document.querySelector(`.weight`).value = '';
 });
+
+const info = document.querySelector(`.info`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseInfo = document.querySelector(`.close-info`);
+const btnsOpenInfo = document.querySelectorAll(`.show-info`);
+
+const openInfo = function () {
+  info.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+  console.log(`open info`);
+};
+const closeInfo = function () {
+  info.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+  console.log(`close info`);
+};
+
+for (let i = 0; i < btnsOpenInfo.length; i++) {
+  btnsOpenInfo[i].addEventListener(`click`, openInfo);
+  btnCloseInfo.addEventListener(`click`, closeInfo);
+  overlay.addEventListener(`click`, closeInfo);
+  console.log(`open info`);
+}
+
+document.addEventListener(`keydown`, function (e) {
+  if (e.key === `Escape` && !info.classList.contains(`hidden`)) {
+    closeModal();
+    console.log(`open info`);
+  }
+});
