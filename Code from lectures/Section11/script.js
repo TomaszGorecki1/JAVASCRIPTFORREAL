838e657bb6eed3337afd0de8411ded3312191e22
Tomasz <tomigunpl@icloud.com>
2022-05-02 22:46:05 +0200

    Strating new project about dom

---
+++ Code from lectures/Section11/script.js
@@ -0,0 +1,73 @@
+'use strict';
+///////////////////////////////////////
+// selecting stuff
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
+///////////////////////////////////////
+// Modal window
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnsOpenModal.forEach(btn => btn.addEventListener(`click`, openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+///////////////////////////////////////
+//BUTTON SCROLLING
+btnScrollTo.addEventListener(`click`, function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  section1.scrollIntoView({ behavior: `smooth` });
+});
+///////////////////////////////////////
+//PAGE NAVIGATION
+
+//////////////////////////////////////////////LECTURES///////////////////////////////////
+
+// console.log(s1coords);
+
+// console.log(`Current scroll(X/Y)`, window.pageXOffset, pageYOffset);
+
+// window.scrollTo(
+//   s1coords.left + window.pageXOffset,
+//   s1coords.top + window.pageYOffset
+// );
+// window.scrollTo({
+//   left: s1coords.left + window.pageXOffset,
+//   top: s1coords.top + window.pageYOffset,
+//   behavior: `smooth`,
+// });
+
+// //rgb(255,255,255)
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// document.querySelector(`.nav__link`).addEventListener(`click`, function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log(`link`, e.target);
+//   // e.stopPropagation();
+// });
+// document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log(`container`, e.target);
+// });
+// document.querySelector(`.nav`).addEventListener(`click`, function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log(`nav`, e.target);
+// });
